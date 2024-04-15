2f72d365f9e50182b59aaba13279df6c06d9a762
Suchir Kavi <suchirkavi@gmail.com>
2024-04-15 06:47:11 -0700

    disable zellij nushell integrations

--- home/tui/zellij.nix
+++ home/tui/zellij.nix
@@ -6,21 +6,22 @@
       enableFishIntegration = true;
     };
   };
-  programs.nushell.extraConfig = ''
-    # auto start zellij
-    # except when in emacs or zellij itself
-    if (not ("ZELLIJ" in $env)) and (not ("INSIDE_EMACS" in $env)) {
-      if "ZELLIJ_AUTO_ATTACH" in $env and $env.ZELLIJ_AUTO_ATTACH == "true" {
-        ^zellij attach -c
-      } else {
-        ^zellij
-      }
+  # nushell integrations
+  # programs.nushell.extraConfig = ''
+  #   # auto start zellij
+  #   # except when in emacs or zellij itself
+  #   if (not ("ZELLIJ" in $env)) and (not ("INSIDE_EMACS" in $env)) {
+  #     if "ZELLIJ_AUTO_ATTACH" in $env and $env.ZELLIJ_AUTO_ATTACH == "true" {
+  #       ^zellij attach -c
+  #     } else {
+  #       ^zellij
+  #     }
 
-      # Auto exit the shell session when zellij exit
-      $env.ZELLIJ_AUTO_EXIT = "false" # disable auto exit
-      if "ZELLIJ_AUTO_EXIT" in $env and $env.ZELLIJ_AUTO_EXIT == "true" {
-        exit
-      }
-    }
-  '';
+  #     # Auto exit the shell session when zellij exit
+  #     $env.ZELLIJ_AUTO_EXIT = "false" # disable auto exit
+  #     if "ZELLIJ_AUTO_EXIT" in $env and $env.ZELLIJ_AUTO_EXIT == "true" {
+  #       exit
+  #     }
+  #   }
+  # '';
 }
