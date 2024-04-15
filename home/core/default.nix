0be2b94c5a70dc77aa16c2327d83dbbc54e92145
Suchir Kavi <suchirkavi@gmail.com>
2024-04-15 07:53:22 -0700

    add fzf and nix-your-shell

--- home/core/default.nix
+++ home/core/default.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   imports = [
+    ./fzf.nix
     ./git.nix
   ];
 
--- home/tui/fish.nix
+++ home/tui/fish.nix
@@ -1,7 +1,13 @@
-{
+{pkgs, ...}: {
+  home.packages = with pkgs; [nix-your-shell];
   programs = {
     fish = {
       enable = true;
+      interactiveShellInit = ''
+        if command -q nix-your-shell
+          nix-your-shell fish | source
+        end
+      '';
     };
   };
 }
