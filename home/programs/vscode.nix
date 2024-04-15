ed2551eb339ef4af8cc6fa273473642bfb86bcdd
Suchir Kavi <suchirkavi@gmail.com>
2024-04-14 22:19:19 -0700

    add nix stuff to vscode

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -1,14 +1,23 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    alejandra
+    nil
+  ];
+
   programs = {
     vscode = {
       enable = true;
 
+      userSettings = {
+        "editor.formatOnSave" = true;
+        "nix.enableLanguageServer" = true;
+        "nix.formatterPath" = "alejandra";
+        "nix.serverPath" = "nil";
+      };
+
       extensions = with pkgs.vscode-extensions; [
         rust-lang.rust-analyzer
+        jnoortheen.nix-ide
       ];
     };
   };
