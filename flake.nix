d43fd4e0c24b6635099a47bcf9821a4c490d7f39
Suchir Kavi <suchirkavi@gmail.com>
2024-07-05 21:34:04 -0700

    23.11 -> 24.05

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Suchir's NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-23.11";
+      url = "github:nix-community/nixvim/nixos-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     rust-overlay.url = "github:oxalica/rust-overlay";
