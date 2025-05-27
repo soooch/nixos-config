4638da565b5bd320c853db21e67d1f5a3c6f8118
Suchir Kavi <suchirkavi@gmail.com>
2025-05-26 17:13:41 -0700

    24.11 -> 25.05

--- home/gui/vscode.nix
+++ home/gui/vscode.nix
@@ -10,41 +10,43 @@
     vscode = {
       enable = true;
 
-      userSettings = {
-        "editor.formatOnSave" = true;
-        "editor.inlayHints.enabled" = "offUnlessPressed";
+      profiles.default = {
+        userSettings = {
+          "editor.formatOnSave" = true;
+          "editor.inlayHints.enabled" = "offUnlessPressed";
 
-        "nix.enableLanguageServer" = true;
-        "nix.formatterPath" = "alejandra";
-        "nix.serverPath" = "nil";
-        "nix.serverSettings" = {
-          "nil" = {
-            "formatting"."command" = ["alejandra"];
-            "nix"."flake"."autoEvalInputs" = true;
+          "nix.enableLanguageServer" = true;
+          "nix.formatterPath" = "alejandra";
+          "nix.serverPath" = "nil";
+          "nix.serverSettings" = {
+            "nil" = {
+              "formatting"."command" = ["alejandra"];
+              "nix"."flake"."autoEvalInputs" = true;
+            };
+            "nixd" = {
+              "formatting"."command" = "alejandra";
+            };
           };
-          "nixd" = {
-            "formatting"."command" = "alejandra";
+
+          "rust-analyzer"."inlayHints" = {
+            "bindingModeHints"."enable" = true;
+            "closureCaptureHints"."enable" = true;
+            "expressionAdjustmentHints"."enable" = "always";
+            "lifetimeElisionHints"."enable" = "always";
+            "rangeExclusiveHints"."enable" = true;
+            "discriminantHints"."enable" = "always";
+            "implicitDrops"."enable" = true;
           };
         };
 
-        "rust-analyzer"."inlayHints" = {
-          "bindingModeHints"."enable" = true;
-          "closureCaptureHints"."enable" = true;
-          "expressionAdjustmentHints"."enable" = "always";
-          "lifetimeElisionHints"."enable" = "always";
-          "rangeExclusiveHints"."enable" = true;
-          "discriminantHints"."enable" = "always";
-          "implicitDrops"."enable" = true;
-        };
+        extensions = with pkgs.vscode-extensions; [
+          rust-lang.rust-analyzer
+          jnoortheen.nix-ide
+          arrterian.nix-env-selector
+          github.copilot
+          bazelbuild.vscode-bazel
+        ];
       };
-
-      extensions = with pkgs.vscode-extensions; [
-        rust-lang.rust-analyzer
-        jnoortheen.nix-ide
-        arrterian.nix-env-selector
-        github.copilot
-        bazelbuild.vscode-bazel
-      ];
     };
   };
 }
--- modules/gpu/amd/default.nix
+++ modules/gpu/amd/default.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{pkgs, ...}: {
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
@@ -23,11 +19,17 @@
     ROC_ENABLE_PRE_VEGA = "1";
   };
 
-  boot.kernelPatches = [
-    {
-      # https://lore.kernel.org/all/2025022644-blinked-broadness-c810@gregkh/
-      name = "revert cve fix which broke chromium rendering for amdgpu";
-      patch = ./0001-Revert-libfs-Use-d_children-list-to-iterate-simple_o.patch;
-    }
-  ];
+  boot.kernelPatches =
+    # https://lore.kernel.org/all/2025022644-blinked-broadness-c810@gregkh/
+    # https://issuetracker.google.com/issues/396434686?pli=1
+    [
+      {
+        name = "revert fix for libfs commit i actually want to revert";
+        patch = ./0001-Revert-libfs-Fix-duplicate-directory-entry-in-offset.patch;
+      }
+      {
+        name = "revert cve fix which broke chromium rendering for amdgpu";
+        patch = ./0002-Revert-libfs-Use-d_children-list-to-iterate-simple_o.patch;
+      }
+    ];
 }
