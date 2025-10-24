41a10102ee153b23d8bfceba316e52b5d16127ff
Suchir Kavi <suchirkavi@gmail.com>
2025-10-23 19:45:37 -0700

    zram: tune sysctl

--- modules/zram.nix
+++ modules/zram.nix
@@ -1,3 +1,9 @@
 {
   zramSwap.enable = true;
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
+  };
 }
