1fa7e213cf62be571819aa2aaebf20036247733e
Suchir Kavi <suchirkavi@gmail.com>
2025-11-29 23:52:02 -0800

    update flake inputs
    switch to new ghostty rose-pine theme name

--- modules/home-manager/modules/tui/ghostty.nix
+++ modules/home-manager/modules/tui/ghostty.nix
@@ -1,7 +1,7 @@
 {inputs, ...}: {
   home.packages = [inputs.ghostty.packages.x86_64-linux.default];
   xdg.configFile."ghostty/config".text = ''
-    theme = rose-pine
+    theme = "Rose Pine"
     font-size = 11
   '';
 }
