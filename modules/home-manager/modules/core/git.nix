7bf8612324fd8409320794b9cb634d2cde4f27fb
Suchir Kavi <suchirkavi@gmail.com>
2025-12-05 18:53:24 -0800

    hm: core: git: nicer config

--- modules/home-manager/modules/core/git.nix
+++ modules/home-manager/modules/core/git.nix
@@ -7,8 +7,30 @@
       userEmail = vars.email;
 
       difftastic.enable = true;
+
+      # https://blog.gitbutler.com/how-git-core-devs-configure-git
       extraConfig = {
+        column.ui = "auto";
+        branch.sort = "-committerdate";
+        tag.sort = "version:refname";
+        init.defaultBranch = "main";
+        diff = {
+          algorithm = "histogram";
+          colorMoved = "plain";
+          mnemonicPrefix = "true";
+          renames = "true";
+        };
+        help.autocorrect = "prompt";
+        rerere = {
+          enabled = "true";
+          autoupdate = "true";
+        };
+        rebase = {
+          autoSquash = "true";
+          autoStash = "true";
+        };
         merge.conflictstyle = "zdiff3";
+        pull.rebase = "true";
       };
     };
   };
