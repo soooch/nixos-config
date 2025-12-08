e72b2826df3063b0bba3f4dfd966630328d6e83c
Suchir Kavi <suchirkavi@gmail.com>
2025-12-07 21:38:54 -0800

    hm: tui: fish: remove nix-your-shell
    resolves #3

--- modules/home-manager/modules/tui/fish.nix
+++ modules/home-manager/modules/tui/fish.nix
@@ -1,13 +1,7 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [nix-your-shell];
   programs = {
     fish = {
       enable = true;
-      interactiveShellInit = ''
-        if command -q nix-your-shell
-          nix-your-shell fish | source
-        end
-      '';
       plugins = [
         {
           name = "fzf.fish";
