8d0b526b76b4b6f6fe0925bdaeddde5c7e25a419
Suchir Kavi <suchirkavi@gmail.com>
2025-12-07 22:14:18 -0800

    hm: tui: bat: use program option

--- modules/home-manager/modules/core/default.nix
+++ modules/home-manager/modules/core/default.nix
@@ -8,7 +8,6 @@
 
   home.packages = with pkgs; [
     # Utils
-    bat
     fd
     wl-clipboard
 
--- modules/home-manager/modules/tui/default.nix
+++ modules/home-manager/modules/tui/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./bat.nix
     ./fish.nix
     ./ghostty.nix
     ./helix.nix
