3cb1add1833968de0377431eb3cf09f974dbe539
Suchir Kavi <suchirkavi@gmail.com>
2026-04-02 00:24:55 -0700

    lovelace: enable tailscale

--- hosts/lovelace/default.nix
+++ hosts/lovelace/default.nix
@@ -7,6 +7,7 @@
     ../../modules/ssd.nix
     ../../modules/logitech.nix
     ../../modules/docker.nix
+    ../../modules/tailscale.nix
     ../../modules/ios.nix
     ../../modules/droidcam.nix
     ../../modules/zram.nix
